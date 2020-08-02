64b9a4f967ee6ab6b2ae3300650820e0e9d053f9
Kevin Fang <58371107+k-fang@users.noreply.github.com>
2020-08-01 22:41:26 -0700

    Add files via upload
    profile data as well as ranked data pulled from api
    todo: pull match history and champion statistics

--- api_test.py
+++ api_test.py
@@ -2,16 +2,61 @@ from riotwatcher import LolWatcher, ApiError
 import pandas as pd
 #import json
 
-api_key = 'I dont wanna get hacked your key here'
+api_key = 'RGAPI-your key here'
 watcher = LolWatcher(api_key)
 region = 'na1'
 
-player = watcher.summoner.by_name(region, 'jankykf')
+player = watcher.summoner.by_name(region, 'RozeZeal')
+#use jankykf for flex and solo stats, ashaowowow for none, and RozeZeal for just solo
 print (player)
 player_name = player ['name']
 player_icon = int (player ['profileIconId'])
 player_level = player ['summonerLevel']
 print(player_name, player_icon, player_level)
 
-player_ranked_stats = watcher.league.by_summoner(region, player['id'])
-print(player_ranked_stats)
+try:
+    player_solo_stats, player_flex_stats = watcher.league.by_summoner(region, player['id'])
+except ValueError:
+    try:
+        if (watcher.league.by_summoner(region, player['id'])[0]['queueType'] == 'RANKED_SOLO_5x5'):
+            player_solo_stats = watcher.league.by_summoner(region, player['id'])
+            player_solo_rank = player_solo_stats [0]['tier']
+            player_solo_rank_num = player_solo_stats [0]['rank']
+            player_solo_wins = int (player_solo_stats [0]['wins'])
+            player_solo_losses = int (player_solo_stats [0]['losses'])
+            player_solo_lp = int (player_solo_stats [0]['leaguePoints'])
+            print (player_solo_rank, player_solo_rank_num, player_solo_wins, player_solo_losses, player_solo_lp)
+
+        elif (watcher.league.by_summoner(region, player['id'])[0]['queueType'] == 'RANKED_FLEX_SR'):                               
+            player_flex_stats = watcher.league.by_summoner(region, player['id'])
+            player_flex_rank = player_flex_stats [0]['tier']
+            player_flex_rank_num = player_flex_stats [0]['rank']
+            player_flex_wins = int (player_flex_stats [0]['wins'])
+            player_flex_losses = int (player_flex_stats [0]['losses'])
+            player_flex_lp = int (player_flex_stats [0]['leaguePoints'])
+            print (player_flex_rank, player_flex_rank_num, player_flex_wins, player_flex_losses, player_flex_lp)
+
+        else:
+            print ('filler, shouldnt ever get here')
+    except IndexError:
+        print('no ranked stats')
+else: 
+    player_solo_rank = player_solo_stats ['tier']
+    player_solo_rank_num = player_solo_stats ['rank']
+    player_solo_wins = int (player_solo_stats ['wins'])
+    player_solo_losses = int (player_solo_stats ['losses'])
+    player_solo_lp = int (player_solo_stats ['leaguePoints'])
+    print (player_solo_rank, player_solo_rank_num, player_solo_wins, player_solo_losses, player_solo_lp)
+
+
+    player_flex_rank = player_flex_stats ['tier']
+    player_flex_rank_num = player_flex_stats ['rank']
+    player_flex_wins = int (player_flex_stats ['wins'])
+    player_flex_losses = int (player_flex_stats ['losses'])
+    player_flex_lp = int (player_flex_stats ['leaguePoints'])
+    print (player_flex_rank, player_flex_rank_num, player_flex_wins, player_flex_losses, player_flex_lp)
+
+
+    print(player_solo_stats, player_flex_stats)
+
+
